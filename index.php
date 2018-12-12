82b61ca5751de1ff38c1d0d933af6255fd769f70
Aruna P <p.aruna1998@gmail.com>
2018-12-12 19:37:17 +0530

    added res

--- index.php
+++ index.php
@@ -14,9 +14,24 @@
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+    <!-- owl carousel css-->
+    <link rel="stylesheet" href="res/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="res/css/owl.theme.default.min.css">
+
+    <!-- animate.css-->
+    <link rel="stylesheet" href="res/css/animate.css">
+
+    <!-- owl carousel js-->
+    <script src="res/js/owl.carousel.min.js"></script>
+
 
     <style>
 
+      .footer {
+        background-color: black;
+        width: 100vw;
+
+      }
 
     </style>
 
@@ -29,6 +44,10 @@
       <div class="">
         <h1>Hello, from Classifile!</html>
       </div>
+    </section>
+
+    <section class='footer'>
+
     </section>
 
     <script type='text/javascript'>
